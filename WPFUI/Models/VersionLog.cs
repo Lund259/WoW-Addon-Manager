063d55aec80e21f4be8fc6d0e41fb8dcfb41e4e3
Lund259 <Jonathan.e.lund@gmail.com>
2019-07-14 18:38:03 +0200

    What's New tab now gets the changelog from github

---
+++ WPFUI/Models/VersionLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFUI.Models
+{
+    class VersionLog
+    {
+        public VersionLog(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
--- WPFUI/ViewModels/ChangelogViewModel.cs
+++ WPFUI/ViewModels/ChangelogViewModel.cs
@@ -3,18 +3,24 @@ using System;
 using System.Collections.Generic;
 using System.Deployment.Application;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using WPFUI.Models;
 
 namespace WPFUI.ViewModels
 {
     class ChangelogViewModel : Screen
     {
         public string CurrentVersion { get; set; }
+        public IEnumerable<VersionLog> ChangeLog { get; set; }
+
 
         public ChangelogViewModel()
         {
             CurrentVersion = $"Your current version: {GetCurrentVersion()}";
+            ChangeLog = GetChangeLog();
         }
 
         private string GetCurrentVersion()
@@ -24,6 +30,39 @@ namespace WPFUI.ViewModels
                : System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
 
             return currentVersionString;
+        }
+
+        private string GetChangeLogText()
+        {
+            string result = "";
+
+            using(WebClient client = new WebClient())
+                result = client.DownloadString("https://raw.githubusercontent.com/Lund259/WoW-Addon-Manager/Development/CHANGELOG.md");
+
+            return result;
+        }
+
+        private List<VersionLog> GetChangeLog()
+        {
+            List<VersionLog> result = new List<VersionLog>();
+
+            //Github md format
+            string changeLogText = GetChangeLogText();
+
+            string[] changeLogLines = changeLogText.Split(new string[] { @"#" }, StringSplitOptions.RemoveEmptyEntries);
+            
+            foreach(string version in changeLogLines)
+            {
+                //Mathces V0.2, V0.2.1
+                Match titleMatch = Regex.Match(version, @"(V)(\*|\d+(\.\d+){0,2}(\.\*)?)");
+
+                string title = titleMatch.Value;
+                string description = version.Substring(titleMatch.Index + titleMatch.Length);
+
+                result.Add(new VersionLog(title, description));
+            }
+
+            return result;
         }
     }
 }
